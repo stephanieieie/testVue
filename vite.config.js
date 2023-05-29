6b7f64aec5e37baf49aa8d970b39eaa3586361c1
Stephanie Zhang <stephanie.q.zhang@kpmg.com>
2023-05-29 08:50:24 +0000

    auto deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/foxy-template',
+  base: process.env.NODE_ENV === 'production'
+    ? '/testVue/'
+    : '/',
   plugins: [vue()],
-  base: '/testVue/'
 })
